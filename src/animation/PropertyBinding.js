5e561c7f26725fa722b1315381bd873e5073ad0e
Thomas Griffiths <griffithst@ocavu.com>
2022-10-04 17:20:32 -0600

    Tweaks to PropertyBinding.js

--- src/animation/PropertyBinding.js
+++ src/animation/PropertyBinding.js
@@ -107,7 +107,11 @@ class PropertyBinding {
 		this.path = path;
 		this.parsedPath = parsedPath || PropertyBinding.parseTrackName( path );
 
-		this.node = PropertyBinding.findNode( rootNode, this.parsedPath.nodeName ) || rootNode;
+		const targetObject = PropertyBinding.findNode( rootNode, this.parsedPath.nodeName );
+
+		// if (!targetObject) return;
+
+		this.node = targetObject;
 
 		this.rootNode = rootNode;
 
@@ -423,7 +427,9 @@ class PropertyBinding {
 
 		if ( ! targetObject ) {
 
-			targetObject = PropertyBinding.findNode( this.rootNode, parsedPath.nodeName ) || this.rootNode;
+			targetObject = PropertyBinding.findNode( this.rootNode, parsedPath.nodeName );
+
+			// if (!targetObject) return;
 
 			this.node = targetObject;
 
